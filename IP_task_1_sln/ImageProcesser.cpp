ab368817ab32f449db7d0498065ded583dcb0f43
lstarosta <lstarosta98@gmail.com>
2018-10-11 20:28:57 +0200

    Created ImageProcesser class

---
+++ IP_task_1_sln/ImageProcesser.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "ImageProcesser.h"
+
+using namespace cimg_library;
+using namespace std;
+
+ImageProcesser::ImageProcesser()
+{
+}
+
+ImageProcesser::~ImageProcesser()
+{
+}
+
+void ImageProcesser::processImage(string imageName, string option, string value = "")
+{
+};
