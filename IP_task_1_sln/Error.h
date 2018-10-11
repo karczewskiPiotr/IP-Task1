a396dfbba7b8df9489ec31a933ac0c2e8cef5a22
karczewskiPiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2018-10-11 16:48:23 +0200

    Exception handling done

---
+++ IP_task_1_sln/Error.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+class Error
+{
+public:
+	Error();
+	~Error();
+
+	//Error functions
+	void InvalidNumberOfArguments();
+	void InvalidOption();
+	void InvalidArguments();
+	void InvalidValue();
+	void OptionNeedsValue();
+
+	//Pointers to error functions
+	void (Error::*invalidNumberOfArguments)() = &Error::InvalidNumberOfArguments;
+	void (Error::*invalidOption)() = &Error::InvalidOption;
+	void (Error::*invalidArguments)() = &Error::InvalidArguments;
+	void (Error::*invalidValue)() = &Error::InvalidValue;
+	void (Error::*optionNeedsValue)() = &Error::OptionNeedsValue;
+};
+
