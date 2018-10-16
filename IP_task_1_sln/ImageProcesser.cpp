2913c169e3b412f94752f8abeec7e73ca83cefa5
karczewskipiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2018-10-16 19:53:35 +0200

    enlarger

--- IP_task_1_sln/ImageProcesser.cpp
+++ IP_task_1_sln/ImageProcesser.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "ImageProcesser.h"
 
 using namespace cimg_library;
@@ -74,23 +73,21 @@ void ImageProcesser::changeContrast(int modifier)
 	}
 }
 
-void ImageProcesser::enlargeImage(double modifier)
+void ImageProcesser::shrinkImage(double modifier)
 {
-	unsigned int enlargedWidth = width * modifier;
-	unsigned int enlargedHeight = height * modifier;
-	float step = 1 / modifier;
+	unsigned int enlargedWidth = (unsigned int)(width / modifier);
+	unsigned int enlargedHeight = (unsigned int)(height / modifier);
+	double step;
 	cimg_library::CImg<unsigned char> enlargedImage(enlargedWidth, enlargedHeight, 1, 3, 0);
 
-	for (unsigned int x = 0; x < enlargedWidth; x++)
+	for (unsigned int y = 0; y < enlargedHeight; y++)
 	{
-		for (unsigned int y = 0; y < enlargedHeight; y++)
+		for (unsigned int x = 0; x < enlargedWidth; x++)
 		{
-			if (step == 1) step = 1 / modifier;
-			//px, py - coordinates of upper left pixel (of 4 adjecent pixels) in the original image
-			int p1x = x / modifier;
-			int p1y = y / modifier;
+			step = (modifier - 1) / 2;
+			int p1x = (int)(x * modifier);
+			int p1y = (int)(y * modifier);
 
-			//positions of 3 neighboring pixels
 			int p2x = p1x + 1;
 			int p2y = p1y;
 
@@ -100,22 +97,56 @@ void ImageProcesser::enlargeImage(double modifier)
 			int p4x = p1x + 1;
 			int p4y = p1y + 1;
 
-			// p1x,p1y	p2x,p2y
-			// p3x,p3y	p4x,p4y
+			for (unsigned int channel = 0; channel < 3; channel++)
+			{
+				double sum1 = ((p2x - (p1x + step)) / (p2x - p1x)) * image(p1x, p1y, channel) + (((p1x + step) - p1x) / (p2x - p1x)) * image(p2x, p2y, channel);
+				double sum2 = ((p4x - (p3x + step)) / (p4x - p3x)) * image(p3x, p3y, channel) + (((p3x + step) - p3x) / (p4x - p3x)) * image(p4x, p4y, channel);
+				double finalSum = ((p3y - (p1y + step)) / (p3y - p1y)) * sum1 + (((p1y + step) - p1y) / (p3y - p1y)) * sum2;
+				enlargedImage(x, y, channel) = truncate((int)finalSum);
+			}
+		}
+	}
+
+	image = enlargedImage;
+}
+
+void ImageProcesser::enlargeImage(double modifier)
+{
+	unsigned int enlargedWidth = (unsigned int)(width * modifier);
+	unsigned int enlargedHeight = (unsigned int)(height * modifier);
+	double step = 1 / modifier;
+	cimg_library::CImg<unsigned char> enlargedImage(enlargedWidth, enlargedHeight, 1, 3, 0);
+
+	for (unsigned int y = 0; y < enlargedHeight; y++)
+	{
+		for (unsigned int x = 0; x < enlargedWidth; x++)
+		{
+			if (step >= 1) step = 1 / modifier;
+			int p1x = (int)(x / modifier);
+			int p1y = (int)(y / modifier);
+
+			int p2x = p1x + 1;
+			int p2y = p1y;
+
+			int p3x = p1x;
+			int p3y = p1y + 1;
+
+			int p4x = p1x + 1;
+			int p4y = p1y + 1;
 
 			for (unsigned int channel = 0; channel < 3; channel++)
 			{
-				float sum1 = ((p2x - (p1x + step)) / (p2x - p1x)) * image(p1x, p1y, channel) + (((p1x + step) - p1x) / (p2x - p1x)) * image(p2x, p2y, channel);
-				float sum2 = ((p4x - (p3x + step)) / (p4x - p3x)) * image(p3x, p3y, channel) + (((p3x + step) - p3x) / (p4x - p3x)) * image(p4x, p4y, channel);
-				float finalSum = ((p3y - (p1y + step)) / (p3y - p1y)) * sum1 + (((p1y + step) - p1y) / (p3y - p1y)) * sum2;
+				double sum1 = ((p2x - (p1x + step)) / (p2x - p1x)) * image(p1x, p1y, channel) + (((p1x + step) - p1x) / (p2x - p1x)) * image(p2x, p2y, channel);
+				double sum2 = ((p4x - (p3x + step)) / (p4x - p3x)) * image(p3x, p3y, channel) + (((p3x + step) - p3x) / (p4x - p3x)) * image(p4x, p4y, channel);
+				double finalSum = ((p3y - (p1y + step)) / (p3y - p1y)) * sum1 + (((p1y + step) - p1y) / (p3y - p1y)) * sum2;
 				enlargedImage(x, y, channel) = truncate((int)finalSum);
 			}
 
 			step += 1 / modifier;
 		}
 	}
-	enlargedImage.display("Processed image preview", false);
-	enlargedImage.save("enlargedImage.bmp");
+	
+	image = enlargedImage;
 }
 
 void ImageProcesser::horizontalFlip()
@@ -190,7 +221,7 @@ void ImageProcesser::processImage()
 		diagonalFlip();
 		break;
 	case shrink:
-
+		shrinkImage(value);
 		break;
 	case enlarge:
 		enlargeImage(value);
