f2acab082821235921f8eefaa73884fb139f9b39
karczewskipiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2018-10-17 12:07:34 +0200

    Update IP_task_1_sln/ImageProcesser.cpp

--- IP_task_1_sln/ImageProcesser.cpp
+++ IP_task_1_sln/ImageProcesser.cpp
@@ -77,31 +77,30 @@ void ImageProcesser::shrinkImage(double modifier)
 {
 	unsigned int enlargedWidth = (unsigned int)(width / modifier);
 	unsigned int enlargedHeight = (unsigned int)(height / modifier);
-	double step;
+	double step, finalSum;
 	cimg_library::CImg<unsigned char> enlargedImage(enlargedWidth, enlargedHeight, 1, 3, 0);
+	int p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y;
 
 	for (unsigned int y = 0; y < enlargedHeight; y++)
 	{
 		for (unsigned int x = 0; x < enlargedWidth; x++)
 		{
 			step = (modifier - 1) / 2;
-			int p1x = (int)(x * modifier);
-			int p1y = (int)(y * modifier);
+			p1x = (int)(x * modifier);
+			p1y = (int)(y * modifier);
 
-			int p2x = p1x + 1;
-			int p2y = p1y;
+			p2x = p1x + 1;
+			p2y = p1y;
 
-			int p3x = p1x;
-			int p3y = p1y + 1;
+			p3x = p1x;
+			p3y = p1y + 1;
 
-			int p4x = p1x + 1;
-			int p4y = p1y + 1;
+			p4x = p1x + 1;
+			p4y = p1y + 1;
 
 			for (unsigned int channel = 0; channel < 3; channel++)
 			{
-				double sum1 = ((p2x - (p1x + step)) / (p2x - p1x)) * image(p1x, p1y, channel) + (((p1x + step) - p1x) / (p2x - p1x)) * image(p2x, p2y, channel);
-				double sum2 = ((p4x - (p3x + step)) / (p4x - p3x)) * image(p3x, p3y, channel) + (((p3x + step) - p3x) / (p4x - p3x)) * image(p4x, p4y, channel);
-				double finalSum = ((p3y - (p1y + step)) / (p3y - p1y)) * sum1 + (((p1y + step) - p1y) / (p3y - p1y)) * sum2;
+				finalSum = 0.25*image(p1x, p1y, channel) + 0.25*image(p2x, p2y, channel) + 0.25*image(p3x, p3y, channel) + 0.25*image(p4x, p4y, channel);
 				enlargedImage(x, y, channel) = truncate((int)finalSum);
 			}
 		}
@@ -114,31 +113,32 @@ void ImageProcesser::enlargeImage(double modifier)
 {
 	unsigned int enlargedWidth = (unsigned int)(width * modifier);
 	unsigned int enlargedHeight = (unsigned int)(height * modifier);
-	double step = 1 / modifier;
+	double sum1, sum2, finalSum, step = 1 / modifier;
 	cimg_library::CImg<unsigned char> enlargedImage(enlargedWidth, enlargedHeight, 1, 3, 0);
+	int p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y;
 
 	for (unsigned int y = 0; y < enlargedHeight; y++)
 	{
 		for (unsigned int x = 0; x < enlargedWidth; x++)
 		{
 			if (step >= 1) step = 1 / modifier;
-			int p1x = (int)(x / modifier);
-			int p1y = (int)(y / modifier);
+			p1x = (int)(x / modifier);
+			p1y = (int)(y / modifier);
 
-			int p2x = p1x + 1;
-			int p2y = p1y;
+			p2x = p1x + 1;
+			p2y = p1y;
 
-			int p3x = p1x;
-			int p3y = p1y + 1;
+			p3x = p1x;
+			p3y = p1y + 1;
 
-			int p4x = p1x + 1;
-			int p4y = p1y + 1;
+			p4x = p1x + 1;
+			p4y = p1y + 1;
 
 			for (unsigned int channel = 0; channel < 3; channel++)
 			{
-				double sum1 = ((p2x - (p1x + step)) / (p2x - p1x)) * image(p1x, p1y, channel) + (((p1x + step) - p1x) / (p2x - p1x)) * image(p2x, p2y, channel);
-				double sum2 = ((p4x - (p3x + step)) / (p4x - p3x)) * image(p3x, p3y, channel) + (((p3x + step) - p3x) / (p4x - p3x)) * image(p4x, p4y, channel);
-				double finalSum = ((p3y - (p1y + step)) / (p3y - p1y)) * sum1 + (((p1y + step) - p1y) / (p3y - p1y)) * sum2;
+				sum1 = ((p2x - (p1x + step)) / (p2x - p1x)) * image(p1x, p1y, channel) + (((p1x + step) - p1x) / (p2x - p1x)) * image(p2x, p2y, channel);
+				sum2 = ((p4x - (p3x + step)) / (p4x - p3x)) * image(p3x, p3y, channel) + (((p3x + step) - p3x) / (p4x - p3x)) * image(p4x, p4y, channel);
+				finalSum = ((p3y - (p1y + step)) / (p3y - p1y)) * sum1 + (((p1y + step) - p1y) / (p3y - p1y)) * sum2;
 				enlargedImage(x, y, channel) = truncate((int)finalSum);
 			}
 
