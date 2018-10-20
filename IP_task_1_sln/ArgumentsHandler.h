43a9b412aa9d31831649f826176fbfeb7598962e
lukasz-starosta <lstarosta98@gmail.com>
2018-10-20 15:00:24 +0200

    Final tweaks and help - Łukasz

--- IP_task_1_sln/ArgumentsHandler.h
+++ IP_task_1_sln/ArgumentsHandler.h
@@ -1,7 +1,5 @@
 #pragma once
-#include <iostream>
 #include <string>
-#include <algorithm>
 
 class ArgumentsHandler
 {
--- IP_task_1_sln/Error.cpp
+++ IP_task_1_sln/Error.cpp
@@ -1,5 +1,5 @@
 #include "Error.h"
-
+#include <iostream>
 
 Error::Error()
 {
